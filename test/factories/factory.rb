f274f2847c1e2282f33b94b8dcf4e86dcff20104
Tad Preston <tad.preston@nokia.com>
2012-06-05 22:52:14 -0500

    added the factory

---
+++ test/factories/factory.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:email) { |n| "email#{n}@factory.com" }
+    sequence(:employee_id)
+    first_name 'Walt'
+    last_name  'Disney'
+    sequence(:username) { |n| "user#{n}" } 
+    password 'supersecret'
+    role User::ROLE_FRONT_DESK_STAFF
+  end
+end
