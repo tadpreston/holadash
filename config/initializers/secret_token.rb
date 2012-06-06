23dfd643919a5f7a3633c8d6a84d3cef985f9184
Tad Preston <tad.preston@nokia.com>
2012-06-05 21:30:04 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portal::Application.config.secret_token = '743c3bca0e983d30f8ec85ce605fd2d755706ecaa410afeb5054d24bce312319cb28f8f7edee0588df581fba9750896c077a47d1f9e5d1d0629aa49d2cd2405a'
