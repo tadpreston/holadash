02db8a759b95237bc1d10eb6c71557d0a96a34a7
Tad Preston <tad.preston@nokia.com>
2012-06-06 23:53:56 -0500

    fixed problems when role column was changed to roles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.normal
 
     respond_to do |format|
       format.html # index.html.erb
