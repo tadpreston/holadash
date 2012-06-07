44e874b447dabc89c54262059dde32c10ede66eb
Tad Preston <tad.preston@nokia.com>
2012-06-06 21:06:06 -0500

    added the sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Portal::Application.routes.draw do
-  get "welcome/index"
+
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :users
+  resources :sessions
 
   root :to => 'welcome#index'
 
