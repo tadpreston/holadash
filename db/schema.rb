2fc4f6d76ee89995af9813221e35cec0a03ba145
Tad Preston <tad.preston@nokia.com>
2012-06-06 20:51:39 -0500

    added password reset columns to the user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120607012255) do
+ActiveRecord::Schema.define(:version => 20120607015043) do
 
   create_table "clubs", :force => true do |t|
     t.string   "name"
@@ -35,8 +35,10 @@ ActiveRecord::Schema.define(:version => 20120607012255) do
     t.string   "password_digest"
     t.string   "roles"
     t.string   "employee_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",             :null => false
+    t.datetime "updated_at",             :null => false
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
 end
