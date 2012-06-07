16502f342e71a3c7424925e6111c00c6f8b8c450
Tad Preston <tad.preston@nokia.com>
2012-06-06 21:57:20 -0500

    added the login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authorize
+
+  private
+
+  def current_user
+    @current_user ||= User.find session[:user_id] unless session[:user_id].blank?
+  end
+
+  def authorize
+    if current_user.nil?
+      session[:referrer] = request.referrer
+      redirect_to login_url 
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,22 @@
-
 class SessionsController < ApplicationController
+  skip_before_filter :authorize
+
   def new
+  end
+
+  def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "You have successfully logged in!"
+    else
+      flash.now.alert = "E-mail and/or password is invalid"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path, notice: "You have been logged out"
   end
 end
