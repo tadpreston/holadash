4be3e6a233a757dc81cba51b54f17b910d852c8f
Tad Preston <tad.preston@nokia.com>
2012-06-19 15:56:19 -0500

    refactored even further

--- app/models/sys_log.rb
+++ app/models/sys_log.rb
@@ -8,25 +8,7 @@ class SysLog < ActiveRecord::Base
   TYPE_DEBUG = "DEBUG"
 
 
-  def info(actioner, message)
-    save_log_details(TYPE_INFO, actioner, message)
-  end
-  
-  def warning(actioner, message)
-    save_log_details(TYPE_WARNING, actioner, message)
-  end
-  
-  def error(actioner, message)
-    save_log_details(TYPE_ERROR, actioner, message)
-  end
-  
-  def debug(actioner, message)
-    save_log_details(TYPE_DEBUG, actioner, message)
-  end
-  
-  private
-  
-  def save_log_details(message_type, actioner, message)
+  def add_log(message_type, actioner, message)
     self.message_type = message_type
     self.message = message
     self.actioned_by = actioner
--- lib/sys_logger/sys_logger.rb
+++ lib/sys_logger/sys_logger.rb
@@ -4,13 +4,7 @@ module SysLogger
 
   def method_missing(method, *args)
     if method.to_s =~ /^log_/  # if the method called is a log method
-      log_type = method.slice(/[^_]+$/)
-      sys_log = sys_logs.new
-      if sys_log.respond_to? log_type
-        sys_log.send "#{log_type}", args[0], args[1]
-      else
-        sys_log.info(args[0], args[1])
-      end
+      sys_logs.new.add_log method.slice(/[^_]+$/).upcase, args[0], args[1]
     else
       super
     end
