c7bb39c6fb142d7138af6476554ff0210f253f3c
Tad Preston <tad.preston@nokia.com>
2012-06-22 16:06:00 -0500

    added mailer smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,13 @@ Portal::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { host: "http://severe-mountain-2159.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'baci.lindsaar.net',
+    user_name:            'tadpreston@gmail.com',
+    password:             'me1indap',
+    authentication:       'plain',
+    enable_starttls_auto: true }
 end
