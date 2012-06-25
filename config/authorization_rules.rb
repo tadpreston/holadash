45277dda71df90ba588f5e20c6767737686620a7
Tad Preston <tad.preston@nokia.com>
2012-06-25 10:46:19 -0500

    added title and description to the roles

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,9 +1,13 @@
 authorization do
   role :guest do
+    title "Guest"
+    description "The role all user have before they log in"
     has_permission_on :welcome, to: :index
   end
 
   role :system_admin do
+    title "System Administrator"
+    description "The highest role that can be assigned to a user in the system"
     includes :guest
     has_permission_on :administration, to: [:index]
     has_permission_on :administration_users, to: [:index,:show,:new,:create,:edit,:update]
@@ -12,6 +16,8 @@ authorization do
   end
 
   role :root do
+    title "Root"
+    description "The top level role that should not be given to anyone except the root user, instead use system_admin"
     has_omnipotence
   end
 end
